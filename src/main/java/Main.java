f2fe3a86db8e6180234b1aa763b83b0b8cf0a196
oahmed105 <oahmed10598@gmail.com>
2023-06-29 19:53:24 -0400

    Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        String line = input.nextLine();
+
     }
 }
