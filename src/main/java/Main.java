99be837bb01cee45d44e3ed444742a2896f5e770
oahmed105 <oahmed10598@gmail.com>
2023-06-29 20:35:29 -0400

    Getting somewhere

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  * Created by iyasuwatts on 10/17/17.
@@ -6,9 +7,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+
+        Random random = new Random();
+        int number = random.nextInt(5) + 1;
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        String line = input.nextLine();
+        int line = input.nextInt();
+
+        do {
+            System.out.print("Enter a number: ");
+            input.nextInt();
+
+        } while (line == number);
+
+
+
+        /*
+        if (line > number) {
+            System.out.println("Too Large");
+        }
+        else if (line < number) {
+            System.out.println("Too Small");
+        }
+        else {
+            System.out.println("Correct Guess");
+        }
+        //System.out.println(number);
+        */
 
     }
 }
